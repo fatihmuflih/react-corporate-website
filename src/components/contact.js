ee36a2b2ed0593cc9e05495cf1b8d4cb4c8a5e3c
fatihmuflih <fatihmuflih06@gmail.com>
2024-10-10 00:38:33 +0700

    add all files and folders

---
+++ src/components/contact.js
@@ -0,0 +1,62 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function AppContact() {
+  return (
+    <section id="contact" className="block contact-block">
+      <Container fluid>
+        <div className="title-holder">
+          <h2>Contact us</h2>
+          <div className="subtitle">get connected with us</div>
+        </div>
+        <Form className='contact-form'>
+          <Row>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Enter your full name" required />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Enter your email address" required />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="tel" placeholder="Enter your contact number" required />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <Form.Control as="textarea" placeholder="Enter your contact message" required />
+            </Col>
+          </Row>
+          <div className='btn-holder'>
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
+      </Container>
+      <div className='google-map'>
+        <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126646.20961854198!2d112.63011054613783!3d-7.275611975292059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbf8381ac47f%3A0x3027a76e352be40!2sSurabaya%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1727284987178!5m2!1sid!2sid"></iframe>
+      </div>
+      <Container fluid>
+        <div className='contact-info'>
+          <ul>
+            <li>
+              <i className="fas fa-envelope"></i>
+              hello@domain.com
+            </li>
+            <li>
+              <i className="fas fa-phone"></i>
+              000-000-0000
+            </li>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              Surabaya, Indonesia
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default AppContact;
